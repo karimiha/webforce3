68d9b77cc850587eca03275fbf87669960f28e0e
karimiha <karim.ihadadene@lepoles.com>
2017-04-27 10:32:46 +0200

    responsive

--- 09-js/asset/js/15.js
+++ 09-js/asset/js/15.js
@@ -1,27 +1,27 @@
  // on demande au user son pseudo
  var monForm = document.getElementById("monFormulaire");
-  
+ var membres = document.getElementById ("membres");
    
 // je vérifie si le pseudo saisie existe dans la base de donnéés
 
 
-    onkeyup="verif()"
+      
+    pseudo.addEventListener("keyup", verif);
+      verif(){
+              var pseudo = document.getElementById("pseudo");
+              var psd = membres.pseudo;
+        for (i =0 ; i <  membres.length ; i ++ )
+            if ( psd == pseudo);
+                {
+                var notif=document.getElementsByClassName("pseudoError");
+                }
+                else
+                {
+                var notif="bon pseudo vous pouvez submittez";
+                }
 
-    verif(){
-          var pseudo = document.getElementById("pseudo");
-          var psd = membres.pseudo;
-        for (i =0 ; i< )
-    if ( psd == pseudo);
-    {
-    var notif="pseudoerror";
-    }
-    else
-    {
-    var notif="bon pseudo vous pouvez submitter";
-    }
-
-     return notif;
-         }
+                 return notif;
+                }
  
     
         
